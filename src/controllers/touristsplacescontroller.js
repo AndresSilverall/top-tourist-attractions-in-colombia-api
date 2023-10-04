f3168d5baaaf110cedcc9bcc2db1eed78edbbb3c
Andres F. Silvera <andresfsilverall0109@gmail.com>
2023-10-03 20:38:46 -0500

    controlador de encontrar lugar por id agregado

--- src/controllers/touristsplacescontroller.js
+++ src/controllers/touristsplacescontroller.js
@@ -1,33 +1,8 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
 const Places = require("../models/places");
 
 
 const app = express()
-
-
-const places = [
-
-    {
-        "id": 1,
-        "name": "Castillo san felipe",
-        "city": "Cartagena",
-        "location": "Bolivar" 
-    },
-    {
-        "id": 2,
-        "name": "Aleta de tiburon",
-        "city": "Barranquilla",
-        "location": "Atlantico"
-    },
-    {
-        "id": 3,
-        "mame": "Plaz bolivar",
-        "city": "Bogota",
-        "location": "Cundinamarca"
-    }
-]
 
 
 exports.getAllData = async (req, res) => {
@@ -44,26 +19,3 @@ exports.getAllData = async (req, res) => {
     }
 }
 
-exports.showAllTouristPLaces = (req, res) => {
-    res.json(places)
-}
-
-exports.getPLaceByid = (req, res) => {
-    const getPlace = places.find(i => i.id == req.params.id)
-    if (!getPlace) {
-        res.status(400).json(
-            {
-                "Message": "Place not found!"
-            }
-        )
-    } else {
-        res.json(getPlace)
-    }
-}
-
-
-exports.addplace = (req, res) => {
-    const data = req.body;
-    places.push(data)
-    res.json(places)
-}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const dataConroller = require("../controllers/touristsplacescontroller");
+const getPLaceController = require("../controllers/getPlaceController")
 const router = express.Router();
 
 
 // llamar al controlador que se encarga de mostrar todos los lugares.
 router.get("/", dataConroller.getAllData)
-router.get("/:id", dataConroller.getPLaceByid)
-router.post("/", dataConroller.addplace)
+router.get("/:id", getPLaceController.getPlaceByID)
+
 
 module.exports = router;
